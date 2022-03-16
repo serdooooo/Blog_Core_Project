1d6ca384e5afa2840e09a9f8cb073ef77e537254
serdooooo <karakurtserdar19@gmail.com>
2022-03-16 03:27:35 +0300

    View

---
+++ Blog/ViewComponents/About/AboutList.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.ViewComponents.About
+{
+    public class AboutList : ViewComponent
+    {
+        AboutManager ab = new AboutManager(new EfAboutDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = ab.TGetList();
+            return View(values);
+        }
+    }
+}
